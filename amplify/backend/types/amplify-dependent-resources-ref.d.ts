9b102dbab1daced494f8cb09b1eb7abe540c92db
ranjurave <ranjurave@gmail.com>
2023-09-10 14:03:19 +1200

    note adding done

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ranjuraveportfolio": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "ranjuraveportfolio466a6442": {
       "AppClientID": "string",
